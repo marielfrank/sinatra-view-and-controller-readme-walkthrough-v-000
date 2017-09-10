72c49d99a7f18b72730209b1f47c3b4f3c654678
Mariel F <marielbeth@gmail.com>
2017-09-10 14:07:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,10 +8,11 @@ class App < Sinatra::Base
   post '/reverse' do
     original_string = params["string"]
     reversed_string = original_string.reverse
+
     erb :reversed
   end
 
   get '/friends' do
-    
+
   end
 end
